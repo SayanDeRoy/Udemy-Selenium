b219108f682435deb4f22c6dd85f1b3137ba454c
SayanDeRoy <sayan.de.roy@gmail.com>
2018-06-16 21:24:03 +0530

    Git Push

---
+++ selenium-feature/src/test/java/seleniumTest/Dummy1.java
@@ -0,0 +1,12 @@
+package seleniumTest;
+
+import org.testng.annotations.Test;
+
+public class Dummy1 {
+	
+	@Test
+	public void smoke()
+	{
+		System.out.println("Smoke");
+	}
+}
---
+++ selenium-feature/src/test/java/seleniumTest/Dummy2.java
@@ -0,0 +1,12 @@
+package seleniumTest;
+
+import org.testng.annotations.Test;
+
+public class Dummy2 {
+	
+	@Test
+	public void regression()
+	{
+		System.out.println("Regression");
+	}
+}
